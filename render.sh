9725021c0d3b6030d40c8fbf36cf3c464132c799
Diego Oliveira <diego.dso123@gmail.com>
2023-07-19 07:59:23 +0800

    new helm version

--- render.sh
+++ render.sh
@@ -3,3 +3,6 @@ cp -pr *.yaml  templates/ repo/helm-frankenstein
 helm package repo/helm-frankenstein --destination repo/
 helm repo index repo/
 rm -rf repo/helm-frankenstein/
+git add .
+git commit -m "new helm version"
+git push origin main
